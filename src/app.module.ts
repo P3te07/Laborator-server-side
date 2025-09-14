e6d7e378fa5e0dd9286c0fbe2c2e594cac563882
P3te07 <petruodobescu@gmail.com>
2025-09-14 21:01:34 +0300

    Add modul users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EquipmentsModule } from './equipments/equipments.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [EquipmentsModule],
+  imports: [EquipmentsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
