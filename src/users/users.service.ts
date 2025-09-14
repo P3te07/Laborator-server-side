b5265dde2e7a9cc29961bbd388d65dc3f5a37f28
P3te07 <petruodobescu@gmail.com>
2025-09-14 21:09:00 +0300

    Users hard-coded + controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'Admin', role: 'admin' },
+    { id: 2, name: 'John', role: 'user' },
+  ];
+
+  findAll() { return this.users; }
+  findOne(id: number) { return this.users.find(u => u.id === id); }
+}
