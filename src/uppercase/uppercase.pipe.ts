3ddc53059d8310530ae921adf6951f9aa435cab2
P3te07 <petruodobescu@gmail.com>
2025-09-17 10:31:04 +0300

    Commit final

--- src/uppercase/uppercase.pipe.ts
+++ src/uppercase/uppercase.pipe.ts
@@ -3,13 +3,10 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 @Injectable()
 export class UppercasePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-  console.log('Before:', value);
-  if (typeof value === 'string') {
-    const upper = value.toUpperCase();
-    console.log('After:', upper);
-    return upper;
+    console.log('Before transform:', value); 
+    const transformed = typeof value === 'string' ? value.toUpperCase() : value;
+    console.log('After transform:', transformed); 
+    return transformed;
   }
-  return value;
 }
 
-}
